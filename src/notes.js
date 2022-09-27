176e6e357ba9bb4150afdcc9deeb3547e44b37eb
Shinagawa-monkey <59675339+Shinagawa-monkey@users.noreply.github.com>
2022-09-26 22:47:09 -0500

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,52 @@
+const notes = [
+  {
+    key: 1,
+    title: "Delegation",
+    content:
+      "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+  },
+  {
+    key: 2,
+    title: "Loops",
+    content:
+      "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat."
+  },
+  {
+    key: 3,
+    title: "Arrays",
+    content:
+      "Q. Why did the programmer quit his job? A. Because he didn't get arrays."
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content:
+      "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+  },
+  {
+    key: 5,
+    title: "Programmers vs. Cats",
+    content:
+      "Q: What do cats and programmers have in common? A: When either one is unusually happy and excited, an appropriate question would be: did you find a bug?"
+  },
+  {
+    key: 6,
+    title: "Inheritance",
+    content:
+      "Q: Whats the object-oriented way to become wealthy? A: Inheritance”"
+  },
+  {
+    key: 7,
+    title: "SQL query",
+    content:
+      "A SQL query goes into a bar, walks up to two tables and asks, “Can I join you?”"
+  },
+  {
+    key: 8,
+    title: "Monkeys & programs",
+    content:
+      "If you put a million monkeys at a million keyboards, one of them will eventually write a Java program. The rest of them will write Perl programs."
+  }
+];
+
+export default notes;
